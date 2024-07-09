9629b55f422e6aa81e86aa136909eb9f6cfa214b
dongykung <kdkd422@naver.com>
2024-07-09 16:44:55 +0900

    스플래쉬 - hiltEntryPoint 적용

---
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.dkproject.presentation.Activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("heelo")
+            Button(onClick = { /*TODO*/ }) {
+                Text("click me")
+            }
+        }
+    }
+}
