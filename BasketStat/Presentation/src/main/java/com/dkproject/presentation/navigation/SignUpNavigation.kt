54bf0c866033c011579b6632f2dffa46a394a880
dongykung <kdkd422@naver.com>
2024-07-16 15:02:26 +0900

    [Add] - 회원가입 네비게이션

---
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/navigation/SignUpNavigation.kt
@@ -0,0 +1,10 @@
+package com.dkproject.presentation.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SignUpNavigation() {
+    Text(text = "Sign Up")
+}
