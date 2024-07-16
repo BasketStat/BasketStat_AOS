b97af79fb4f17b66485fbe86a0d0b2276da00839
dongykung <kdkd422@naver.com>
2024-07-16 15:02:52 +0900

    [DI] - 파이어스토어

--- BasketStat/Data/src/main/java/com/dkproject/data/di/FirebaseModule.kt
+++ BasketStat/Data/src/main/java/com/dkproject/data/di/FirebaseModule.kt
@@ -3,6 +3,8 @@ package com.dkproject.data.di
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,5 +20,11 @@ class FirebaseModule {
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth {
         return Firebase.auth
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return Firebase.firestore
     }
 }
