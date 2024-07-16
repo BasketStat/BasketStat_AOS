8808b0f7e5a915fde0946892c20a9acbc73137e7
dongykung <kdkd422@naver.com>
2024-07-16 15:01:11 +0900

    [Feat] - 로그인 뷰모델(구글 로그인 구현)

--- BasketStat/Presentation/src/main/java/com/dkproject/presentation/ui/screen/login/LoginViewModel.kt
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/ui/screen/login/LoginViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dkproject.domain.usecase.signup.CheckExistUserUseCase
 import com.google.firebase.auth.FirebaseAuth
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
@@ -11,6 +12,9 @@ import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,12 +22,15 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val auth: FirebaseAuth,
+    private val checkExistUserUseCase: CheckExistUserUseCase,
     @ApplicationContext private val context: Context
-): ViewModel() {
+) : ViewModel() {
     companion object {
         const val TAG = "LoginViewModel"
     }
 
+    private val _state = MutableStateFlow(loginUiState())
+    val state = _state.asStateFlow()
     fun kakaoLogin() {
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
@@ -35,8 +42,7 @@ class LoginViewModel @Inject constructor(
         UserApiClient.instance.loginWithKakaoAccount(context) { token, error ->
             if (error != null) {
                 Log.e(TAG, "로그인 실패", error)
-            }
-            else if (token != null) {
+            } else if (token != null) {
                 Log.i(TAG, "로그인 성공 ${token.accessToken}")
                 UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
             }
@@ -46,11 +52,27 @@ class LoginViewModel @Inject constructor(
     fun googleLogin() {
         viewModelScope.launch {
             Log.d(TAG, auth.currentUser?.uid.toString())
+            checkExistUserUseCase(userUid = auth.currentUser?.uid.toString()).fold(
+                onSuccess = { exist ->
+                    if (exist) {
+                        _state.update { it.copy(navigation = LoginNavigation.Home) }
+                    } else {
+                        _state.update { it.copy(navigation = LoginNavigation.SignUp) }
+                    }
+                },
+                onFailure = {
+
+                }
+            )
         }
     }
 
     fun appleLogin() {
 
+    }
+
+    fun navigationReset() {
+        _state.update { it.copy(navigation = LoginNavigation.Default) }
     }
 
 
