c23676f49df307d1b183dc5a6b22ea8e927fd0da
dongykung <kdkd422@naver.com>
2024-07-21 01:27:07 +0900

    [Style] - 상태바 수정중

--- BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/HomeActivity.kt
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.dkproject.presentation.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import com.dkproject.presentation.ui.theme.BasketStatTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ class HomeActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             BasketStatTheme {
                 Text(text = "HomeView")
