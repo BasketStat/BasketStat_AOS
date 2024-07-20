91b5bd30497fde027081c558b9d5394bb41f55f3
dongykung <kdkd422@naver.com>
2024-07-20 14:26:00 +0900

    [Chore] - 회원가입 액티비티

--- BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/SignUpActivity.kt
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/SignUpActivity.kt
@@ -3,7 +3,15 @@ package com.dkproject.presentation.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.dkproject.presentation.navigation.SignUpNavigation
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dkproject.presentation.ui.screen.signUp.SignUpScreen
+import com.dkproject.presentation.ui.screen.signUp.SignUpViewModel
+import com.dkproject.presentation.ui.theme.BasketStatTheme
+import com.dkproject.presentation.ui.theme.background
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -12,7 +20,9 @@ class SignUpActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SignUpNavigation()
+            BasketStatTheme {
+                    SignUpScreen()
+            }
         }
     }
 }
