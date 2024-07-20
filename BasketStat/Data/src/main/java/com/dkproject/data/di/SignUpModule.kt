b5f127814434b6f04c35b531bb1b93ff6eccd6df
dongykung <kdkd422@naver.com>
2024-07-20 14:28:29 +0900

    [DI] - 회원가입 모듈 의존성 주입

--- BasketStat/Data/src/main/java/com/dkproject/data/di/SignUpModule.kt
+++ BasketStat/Data/src/main/java/com/dkproject/data/di/SignUpModule.kt
@@ -1,9 +1,15 @@
 package com.dkproject.data.di
 
+import com.dkproject.data.repository.Image.ImageRepositoryImpl
 import com.dkproject.data.repository.SignUp.SignUpRepositoryImpl
+import com.dkproject.domain.repository.ImageRepository
 import com.dkproject.domain.repository.SignUpRepository
+import com.dkproject.domain.usecase.Image.UploadImageUseCase
+import com.dkproject.domain.usecase.Image.UploadImageUseCaseImpl
 import com.dkproject.domain.usecase.signup.CheckExistUserUseCase
 import com.dkproject.domain.usecase.signup.CheckExistUserUseCaseImpl
+import com.dkproject.domain.usecase.signup.SignUpUseCase
+import com.dkproject.domain.usecase.signup.SignUpUseCaseImpl
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -17,6 +23,9 @@ import javax.inject.Singleton
 abstract class SignUpModule {
     @Binds
     abstract fun bindSignUpRepository(impl: SignUpRepositoryImpl): SignUpRepository
+
+    @Binds
+    abstract fun bindImageRepository(impl: ImageRepositoryImpl): ImageRepository
 }
 
 @Module
@@ -26,5 +35,17 @@ object SignUpUseCaseModule {
     @Provides
     fun provideCheckExistUserUseCase(signUpRepository: SignUpRepository): CheckExistUserUseCase {
         return CheckExistUserUseCaseImpl(signUpRepository = signUpRepository)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUploadImageUseCase(imageRepository: ImageRepository) : UploadImageUseCase {
+        return UploadImageUseCaseImpl(uploadImageRepository = imageRepository)
+    }
+
+    @Singleton
+    @Provides
+    fun provideSignUpUseCase(signUpRepository: SignUpRepository) : SignUpUseCase {
+        return SignUpUseCaseImpl(signUpRepository = signUpRepository)
     }
 }
