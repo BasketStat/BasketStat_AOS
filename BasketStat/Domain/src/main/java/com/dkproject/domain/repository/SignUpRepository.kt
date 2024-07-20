1aeda32e20a9a1caafd058bceec8ae596401b3b7
dongykung <kdkd422@naver.com>
2024-07-20 14:27:21 +0900

    [Feat] - 유저 정보 등록 기능 추가

--- BasketStat/Domain/src/main/java/com/dkproject/domain/repository/SignUpRepository.kt
+++ BasketStat/Domain/src/main/java/com/dkproject/domain/repository/SignUpRepository.kt
@@ -1,5 +1,8 @@
 package com.dkproject.domain.repository
 
+import com.dkproject.domain.model.UserData
+
 interface SignUpRepository {
     suspend fun checkExistUser(userUid: String): Result<Boolean>
+    suspend fun setUserData(userData: UserData): Result<Boolean>
 }
