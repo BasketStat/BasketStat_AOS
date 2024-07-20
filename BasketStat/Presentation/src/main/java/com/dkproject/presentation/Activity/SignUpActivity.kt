5f53742a6e2e1ff45f79f0a6df22a2046d2dac27
dongykung <kdkd422@naver.com>
2024-07-21 01:26:50 +0900

    [Style] - 상태바 수정중

--- BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/SignUpActivity.kt
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/SignUpActivity.kt
@@ -3,21 +3,16 @@ package com.dkproject.presentation.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.activity.enableEdgeToEdge
 import com.dkproject.presentation.ui.screen.signUp.SignUpScreen
-import com.dkproject.presentation.ui.screen.signUp.SignUpViewModel
 import com.dkproject.presentation.ui.theme.BasketStatTheme
-import com.dkproject.presentation.ui.theme.background
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class SignUpActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             BasketStatTheme {
