bc4660be3d9972e779b39c34498ea686d70d98f7
dongykung <kdkd422@naver.com>
2024-07-20 16:43:22 +0900

    [Add] - 포지션 데이터

---
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/util/PositionData.kt
@@ -0,0 +1,14 @@
+package com.dkproject.presentation.util
+
+
+
+
+object PositionData {
+    val positions: List<Pair<String, String>> = listOf(
+        "포인트 가드" to "PG",
+        "슈팅 가드" to "SG",
+        "스몰 포워드" to "SF",
+        "파워 포워드" to "PF",
+        "센터" to "C"
+    )
+}
