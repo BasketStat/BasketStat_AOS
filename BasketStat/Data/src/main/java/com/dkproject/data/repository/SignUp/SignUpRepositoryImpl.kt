536e985a932e09e792f5ac600871be8173ce8354
dongykung <kdkd422@naver.com>
2024-07-20 14:27:17 +0900

    [Feat] - 유저 정보 등록 기능 추가

--- BasketStat/Data/src/main/java/com/dkproject/data/repository/SignUp/SignUpRepositoryImpl.kt
+++ BasketStat/Data/src/main/java/com/dkproject/data/repository/SignUp/SignUpRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.dkproject.data.repository.SignUp
 
+import com.dkproject.domain.model.UserData
 import com.dkproject.domain.repository.SignUpRepository
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -10,13 +12,22 @@ class SignUpRepositoryImpl @Inject constructor(
 ): SignUpRepository{
     override suspend fun checkExistUser(userUid: String): Result<Boolean> {
         return try {
-            val snapshot = fireStore.collection("users").whereEqualTo("userUid", userUid).get().await()
+            val snapshot = fireStore.collection("Users").whereEqualTo("userUid", userUid).get().await()
             if(snapshot.isEmpty)
                 Result.success(false)
             else
                 Result.success(true)
         } catch (e: Exception){
                 Result.failure(e)
+        }
+    }
+
+    override suspend fun setUserData(userData: UserData): Result<Boolean> {
+        return try {
+            fireStore.collection("Users").document(userData.userUid).set(userData, SetOptions.merge()).await()
+            Result.success(true)
+        } catch (e: Exception) {
+            Result.failure(e)
         }
     }
 }
