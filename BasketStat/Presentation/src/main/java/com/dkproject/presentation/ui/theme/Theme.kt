8685d5abee5c4d068d62bea6e06389069f1ab1e5
dongykung <kdkd422@naver.com>
2024-07-20 14:20:39 +0900

    [Chore] - 테마

--- BasketStat/Presentation/src/main/java/com/dkproject/presentation/ui/theme/Theme.kt
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/ui/theme/Theme.kt
@@ -9,18 +9,27 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Color(0XFF3A3530),
+    onBackground = Color(0XFFFFFFFF)
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Color(0XFF3A3530),
+    onBackground = Color(0XFFFFFFFF)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -48,6 +57,14 @@ fun BasketStatTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color(0XFF3A3530).toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
