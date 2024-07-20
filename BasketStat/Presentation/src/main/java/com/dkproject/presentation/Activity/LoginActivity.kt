43a34477a18c56c85f81cb5887317daf08a804a1
dongykung <kdkd422@naver.com>
2024-07-20 14:28:11 +0900

    [Chore] - 로그인 액비티비

--- BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/LoginActivity.kt
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/Activity/LoginActivity.kt
@@ -18,6 +18,7 @@ import com.dkproject.presentation.ui.screen.login.GoogleAuthUiClient
 import com.dkproject.presentation.ui.screen.login.LoginNavigation
 import com.dkproject.presentation.ui.screen.login.LoginScreen
 import com.dkproject.presentation.ui.screen.login.LoginViewModel
+import com.dkproject.presentation.ui.theme.BasketStatTheme
 import com.google.android.gms.auth.api.identity.Identity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -28,49 +29,57 @@ class LoginActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val context = LocalContext.current
-            val viewModel = viewModel<LoginViewModel>()
-            val state = viewModel.state.collectAsState().value
+            BasketStatTheme {
+                val context = LocalContext.current
+                val viewModel = viewModel<LoginViewModel>()
+                val state = viewModel.state.collectAsState().value
 
-            LaunchedEffect(state.navigation) {
-                when(state.navigation) {
-                    LoginNavigation.SignUp -> {
-                        context.startActivity(Intent(context, SignUpActivity::class.java))
-                        viewModel.navigationReset()
-                    }
-                    LoginNavigation.Home -> {
-                        context.startActivity(Intent(context, HomeActivity::class.java).apply {
-                            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        })
-                    }
-                    LoginNavigation.Default -> {}
-                }
-            }
+                LaunchedEffect(state.navigation) {
+                    when (state.navigation) {
+                        LoginNavigation.SignUp -> {
+                            context.startActivity(Intent(context, SignUpActivity::class.java))
+                            viewModel.navigationReset()
+                        }
 
-            val googleAuthUiClient by lazy {
-                GoogleAuthUiClient(
-                    context = context,
-                    oneTapClient = Identity.getSignInClient(context)
-                )
-            }
-            val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartIntentSenderForResult()) { result ->
-                if(result.resultCode == RESULT_OK) {
-                    lifecycleScope.launch {
-                        val signInResult = googleAuthUiClient.signInWithIntent(intent = result.data ?: return@launch)
-                        viewModel.googleLogin()
+                        LoginNavigation.Home -> {
+                            context.startActivity(Intent(context, HomeActivity::class.java).apply {
+                                flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            })
+                        }
+
+                        LoginNavigation.Default -> {}
                     }
                 }
-            }
-            LoginScreen(viewModel = viewModel) {
-                lifecycleScope.launch {
-                    val signInIntentSender = googleAuthUiClient.signIn()
-                    launcher.launch(
-                        IntentSenderRequest.Builder(
-                            intentSender = signInIntentSender ?: return@launch
-                        ).build()
+
+                val googleAuthUiClient by lazy {
+                    GoogleAuthUiClient(
+                        context = context,
+                        oneTapClient = Identity.getSignInClient(context)
                     )
                 }
+                val launcher =
+                    rememberLauncherForActivityResult(contract = ActivityResultContracts.StartIntentSenderForResult()) { result ->
+                        if (result.resultCode == RESULT_OK) {
+                            lifecycleScope.launch {
+                                googleAuthUiClient.signInWithIntent(
+                                    intent = result.data ?: return@launch
+                                )
+                                viewModel.googleLogin()
+                            }
+                        }
+                    }
+                LoginScreen(viewModel = viewModel) {
+                    lifecycleScope.launch {
+                        val signInIntentSender = googleAuthUiClient.signIn()
+                        launcher.launch(
+                            IntentSenderRequest.Builder(
+                                intentSender = signInIntentSender ?: return@launch
+                            ).build()
+                        )
+                    }
 
+                }
             }
         }
     }
