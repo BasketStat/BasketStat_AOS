ab7f9f1e4b76f0aa8ccd9982b85c600a4e712b81
dongykung <kdkd422@naver.com>
2024-07-23 15:41:03 +0900

    [Add] - 홈 메뉴 버튼(공용)

---
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/ui/components/HomeMenuButton.kt
@@ -0,0 +1,42 @@
+package com.dkproject.presentation.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dkproject.presentation.ui.theme.recordboxborder
+
+
+@Composable
+fun HomeMenuButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    image: ImageVector? = null,
+    onClick: () -> Unit
+) {
+    Surface(modifier = modifier, shape = RoundedCornerShape(4.dp), color = Color.Transparent, border = BorderStroke(1.dp, recordboxborder)) {
+        Row(
+            modifier = Modifier.clickable {  },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            image?.let {
+                Icon(modifier = Modifier.padding(end = 6.dp),imageVector = it, contentDescription = null)
+            }
+            Text(modifier=Modifier.padding(6.dp),text = text, color = Color.White, fontSize = 22.sp)
+        }
+    }
+}
