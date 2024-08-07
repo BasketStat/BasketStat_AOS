5f6760455dc7ed35d587fef0becce383e1403610
dongykung <kdkd422@naver.com>
2024-08-07 17:11:22 +0900

    [fix] - 데이터 기본값 추가

--- BasketStat/Domain/src/main/java/com/dkproject/domain/model/UserData.kt
+++ BasketStat/Domain/src/main/java/com/dkproject/domain/model/UserData.kt
@@ -1,10 +1,10 @@
 package com.dkproject.domain.model
 
 data class UserData(
-    val userUid: String,
-    val userNickName: String,
-    val userProfileImageUrl: String,
-    val userHeight: Double,
-    val userWeight: Double,
-    val userPosition: String
+    val userUid: String = "",
+    val userNickName: String = "",
+    val userProfileImageUrl: String = "",
+    val userHeight: Double = 0.0,
+    val userWeight: Double = 0.0,
+    val userPosition: String = ""
 )
