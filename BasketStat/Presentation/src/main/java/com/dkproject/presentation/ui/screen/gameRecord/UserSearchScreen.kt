9da5ccf0229854e3f56dd17ce799e183462a600e
dongykung <kdkd422@naver.com>
2024-08-23 13:21:42 +0900

    [feat] - 유저 검색 프로그래스 추가

--- BasketStat/Presentation/src/main/java/com/dkproject/presentation/ui/screen/gameRecord/UserSearchScreen.kt
+++ BasketStat/Presentation/src/main/java/com/dkproject/presentation/ui/screen/gameRecord/UserSearchScreen.kt
@@ -2,6 +2,8 @@ package com.dkproject.presentation.ui.screen.gameRecord
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -20,15 +22,24 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -46,16 +57,21 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.algolia.instantsearch.android.paging3.flow
 import com.dkproject.domain.model.UserData
+import com.dkproject.domain.model.UserRecord
 import com.dkproject.presentation.R
 import com.dkproject.presentation.ui.components.CustomTextField
+import com.dkproject.presentation.ui.components.SelectNumberDialog
 import com.dkproject.presentation.ui.theme.BasketStatTheme
 import com.dkproject.presentation.ui.theme.background
+import com.dkproject.presentation.ui.theme.cardItembg
 import com.dkproject.presentation.ui.theme.searchTextbg
 
 
@@ -63,88 +79,173 @@ import com.dkproject.presentation.ui.theme.searchTextbg
 @Composable
 fun UserSearchScreen(
     modifier: Modifier = Modifier,
-    viewModel: UserSearchViewModel,
+    recordViewModel: RecordViewModel,
+    viewModel: UserSearchViewModel = hiltViewModel(),
+    homeAway: Boolean,
     onBack: () -> Unit = {},
     userClick: (UserDataUiModel) -> Unit = {}
 ) {
+    val recordState by recordViewModel.uiState.collectAsState()
     val hitsPaging = viewModel.hitsPaginator.flow.collectAsLazyPagingItems()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    Scaffold(modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    var dialogVisible by remember { mutableStateOf(false) }
+    val snackbarMessage by viewModel.snackbarMessages.collectAsState()
+    val snackbarHostState = remember { SnackbarHostState() } //스낵바 상태
+    var selectedMember by remember { mutableStateOf(UserDataUiModel()) }
+    LaunchedEffect(key1 = snackbarMessage) {
+        snackbarMessage?.let {
+            val result = snackbarHostState.showSnackbar(
+                message = it,
+                actionLabel = "닫기",
+                duration = SnackbarDuration.Short
+            )
+            when (result) {
+                SnackbarResult.Dismissed -> {}
+                SnackbarResult.ActionPerformed -> {}
+            }
+            viewModel.updateSnackbarClear()
+        }
+    }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             CenterAlignedTopAppBar(
                 title = { Text(text = "선수 검색", color = Color.White) },
-                scrollBehavior = scrollBehavior,
                 colors = TopAppBarDefaults.topAppBarColors(containerColor = background),
                 navigationIcon = {
                     IconButton(onClick = { onBack() }) {
-                        Icon(imageVector = Icons.AutoMirrored.Default.ArrowBack, contentDescription = null, tint = Color.White)
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
                     }
                 }
             )
         }) { innerPadding ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .background(background),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .background(background)
         ) {
-            CustomTextField(
-                modifier = Modifier.fillMaxWidth(0.9f),
-                text = viewModel.searchBoxState.query,
-                placeholder = "선수를 검색해주세요.",
-                onTextChange = {
-                     viewModel.searchBoxState.setText(it, submitQuery = true)
-                },
-                shape = RoundedCornerShape(8.dp),
-                colors = TextFieldDefaults.colors(focusedContainerColor = searchTextbg,unfocusedContainerColor = searchTextbg,
-                    focusedIndicatorColor = Color.Transparent, unfocusedIndicatorColor = Color.Transparent,
-                    focusedTextColor = Color.White,unfocusedTextColor = Color.White),
-                leadingIcon = Icons.Default.Search,
-                isSingleLine = true,
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    imeAction = ImeAction.Search
-                )
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-
-            LazyColumn() {
-                items(hitsPaging.itemSnapshotList.items, key = { it.userUid }) { user ->
-                    UserSearchItem(modifier = Modifier.clickable { userClick(user) },user = user)
+            if (dialogVisible) {
+                SelectNumberDialog(onDismiss = { dialogVisible = false }) { userbackNumber ->
+                    if (recordState.userRecords.find { it.backNumber == userbackNumber } == null) {
+                        recordViewModel.addMember(
+                            userUid = selectedMember.userUid,
+                            userName = selectedMember.userNickName,
+                            backNumber = userbackNumber,
+                            homeaway = homeAway
+                        )
+                        dialogVisible = false
+                        onBack()
+                    } else {
+                        dialogVisible = false
+                        viewModel.updateSnackbarMessage("해당 등 번호는 이미 존재 합니다.")
+                    }
                 }
             }
-        }
-    }
-}
+            if (hitsPaging.loadState.refresh is LoadState.Loading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                //검색 섹션
+                CustomTextField(
+                    modifier = Modifier.fillMaxWidth(0.9f),
+                    text = viewModel.searchBoxState.query,
+                    placeholder = "선수를 검색해주세요.",
+                    onTextChange = {
+                        viewModel.searchBoxState.setText(it, submitQuery = true)
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = searchTextbg,
+                        unfocusedContainerColor = searchTextbg,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        disabledContainerColor = searchTextbg,
+                        errorContainerColor = searchTextbg
+                    ),
+                    leadingIcon = Icons.Default.Search,
+                    isSingleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search
+                    )
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+
+                LazyColumn() {
+                    items(hitsPaging.itemSnapshotList.items, key = { it.userUid }) { user ->
+                        UserSearchItem(
+                            modifier = Modifier,
+                            user = user,
+                            onClick = {
+                                if (recordState.game.usersUid.contains(it.userUid)) {
+                                    viewModel.updateSnackbarMessage("해당 선수가 이미 존재합니다.")
+                                } else {
+                                    selectedMember = it
+                                    dialogVisible = true
+                                }
+                            }
+                        )
+                    }
+                }//endLayColumn
+            }//endColumn
+        } //endBox
+    }//endScaffold
+}//end fun
 
 
 @Composable
 fun UserSearchItem(
     modifier: Modifier = Modifier,
-    user: UserDataUiModel
+    user: UserDataUiModel,
+    onClick: (UserDataUiModel) -> Unit = {}
 ) {
     val context = LocalContext.current
-    Row(modifier = modifier
-        .padding(vertical = 6.dp, horizontal = 16.dp)
-        .fillMaxWidth(),verticalAlignment = Alignment.CenterVertically) {
-        AsyncImage(
-            modifier = Modifier
-                .size(60.dp)
-                .clip(CircleShape),
-            model = ImageRequest.Builder(context)
-                .data(user.userProfileImageUrl)
-                .crossfade(true)
-                .error(R.drawable.basic_image)
-                .build(),
-            contentDescription = "",
-            contentScale = ContentScale.Crop)
-        Spacer(modifier = Modifier.width(12.dp))
-        Text(text = user.userNickName, color = Color.White, fontSize = 16.sp)
-        Spacer(modifier = Modifier.weight(1f))
-        Text(text = user.userPosition, color = Color.White)
+    ElevatedCard(
+        modifier = modifier
+            .padding(vertical = 6.dp)
+            .fillMaxWidth(0.9f)
+            .height(80.dp),
+        colors = CardDefaults.cardColors(containerColor = cardItembg),
+        onClick = { onClick(user) },
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    model = ImageRequest.Builder(context)
+                        .data(user.userProfileImageUrl)
+                        .crossfade(true)
+                        .error(R.drawable.basic_image)
+                        .build(),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(14.dp))
+                Text(text = user.userNickName, color = Color.White, fontSize = 16.sp)
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = user.userPosition, color = Color.White)
+            }
+        }
     }
-
 }
 
 
@@ -153,7 +254,11 @@ fun UserSearchItem(
 private fun UserSearchScreenPreview() {
     BasketStatTheme {
         val viewModel: UserSearchViewModel = viewModel()
-        UserSearchScreen(modifier = Modifier.background(background),
-            viewModel = viewModel)
+        UserSearchScreen(
+            modifier = Modifier.background(background),
+            recordViewModel = RecordViewModel(),
+            homeAway = false,
+            viewModel = viewModel
+        )
     }
 }
